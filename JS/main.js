8d8ab781d54b00311cbb3c8b4939f88abc679e5c
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2022-11-21 16:25:13 +0100

    add

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+
+const { createApp } = Vue;
+
+createApp({
+    
+    data(){
+        return{
+            name: "Piersilvio",
+            foto: "IMG/IMG_8852 2.JPG",
+        }
+    }
+
+}).mount('#app');
